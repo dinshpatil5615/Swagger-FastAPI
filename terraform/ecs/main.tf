bffa7d3f73328778185783c7c376c1da3406e198
Your Name <you@example.com>
2026-04-02 12:43:59 +0530

    commiting main.tf file

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -470,7 +470,7 @@ resource "aws_ecr_repository" "fastapi_repo" {
     encryption_type = "KMS"
   }
 
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
